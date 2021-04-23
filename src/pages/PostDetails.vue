9e7cae19cf0aac4b3b11ed10fdc68216e88d8bcc
ethanboone <eboone2019@gmail.com>
2021-04-23 15:20:56 -0600

    Added route for post page

---
+++ src/pages/PostDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { computed, onMounted, reactive } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default {
+  setup() {
+    const route = useRoute()
+    const state = reactive({
+
+    })
+  }
+}
+</script>
